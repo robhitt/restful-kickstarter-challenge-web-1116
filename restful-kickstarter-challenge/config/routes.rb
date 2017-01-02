f8383815e83dc521ddf700fdddf9962d52c56144
Rob Hitt <robhitt@gmail.com>
2017-01-01 22:54:44 -0500

    delete added to show page and action

--- restful-kickstarter-challenge/config/routes.rb
+++ restful-kickstarter-challenge/config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'projects/', to: 'projects#index'
     get 'projects/new', to: 'projects#new', as: 'new_project'
-    get 'projects/:id', to: 'projects#show', as: 'project'
     get 'projects/:id/edit', to: 'projects#edit', as: 'edit_project'
+    get 'projects/:id', to: 'projects#show', as: 'project'
+    delete 'projects/:id', to: 'projects#destroy'
     post 'projects', to: 'projects#create'
     patch 'projects/:id', to: 'projects#update'
-    delete 'projects/:id', to: 'projects#delete'
+
 end
